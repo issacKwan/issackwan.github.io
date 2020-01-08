e4418b5f9c997ab51fd28e0a72bb5810f3da9613
17594580@qq.com <17594580@qq.com>
2020-01-08 18:11:28 +0800

    Site updated: 2020-01-08 18:11:28

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","4fd88f2d964230be117df0deff37534e"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
